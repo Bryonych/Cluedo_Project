5f4fd86ceb5e9224c1aa2280d098ba285bac6b71
Bryonych <49720607+Bryonych@users.noreply.github.com>
2020-08-03 12:48:11 +1200

    Update parent class

--- src/Card.java
+++ src/Card.java
@@ -7,75 +7,12 @@
 // line 156 "model.ump"
 public class Card
 {
+ private String name;
 
-  //------------------------
-  // MEMBER VARIABLES
-  //------------------------
+public Card(String name){
+  this.name = name;
+}
 
-  //Card Associations
-  private Game game;
 
-  //------------------------
-  // CONSTRUCTOR
-  //------------------------
-
-  public Card(Game aGame)
-  {
-    boolean didAddGame = setGame(aGame);
-    if (!didAddGame)
-    {
-      throw new RuntimeException("Unable to create card due to game. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
-    }
-  }
-
-  //------------------------
-  // INTERFACE
-  //------------------------
-  /* Code from template association_GetOne */
-  public Game getGame()
-  {
-    return game;
-  }
-  /* Code from template association_SetOneToAtMostN */
-  public boolean setGame(Game aGame)
-  {
-    boolean wasSet = false;
-    //Must provide game to card
-    if (aGame == null)
-    {
-      return wasSet;
-    }
-
-    //game already at maximum (21)
-    if (aGame.numberOfCards() >= Game.maximumNumberOfCards())
-    {
-      return wasSet;
-    }
-    
-    Game existingGame = game;
-    game = aGame;
-    if (existingGame != null && !existingGame.equals(aGame))
-    {
-      boolean didRemove = existingGame.removeCard(this);
-      if (!didRemove)
-      {
-        game = existingGame;
-        return wasSet;
-      }
-    }
-    game.addCard(this);
-    wasSet = true;
-    return wasSet;
-  }
-
-  public void delete()
-  {
-    Game placeholderGame = game;
-    this.game = null;
-    if(placeholderGame != null)
-    {
-      placeholderGame.removeCard(this);
-    }
-  }
 
 }
