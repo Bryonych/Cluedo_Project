c21371b3eaf1c7a7ce9bb2c26326f2f7e4407e52
Bryonych <49720607+Bryonych@users.noreply.github.com>
2020-08-04 20:40:18 +1200

    Update toString method

--- src/WeaponCard.java
+++ src/WeaponCard.java
@@ -41,6 +41,6 @@ public class WeaponCard extends Card
   {
     return " ------------------- \n" +
             " Card: " + name + "\n" +
-            " ------------------- ";
+            " -------------------\n";
   }
 }
