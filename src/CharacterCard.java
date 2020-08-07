19394393af5a9756a22279d28b5d0d7bc6f1594c
Bryonych <49720607+Bryonych@users.noreply.github.com>
2020-08-07 14:27:13 +1200

    Added character field

--- src/CharacterCard.java
+++ src/CharacterCard.java
@@ -1,54 +1,43 @@
-/*PLEASE DO NOT EDIT THIS CODE*/
-/*This code was generated using the UMPLE 1.30.0.5074.a43557235 modeling language!*/
-
-
-
-// line 71 "model.ump"
-// line 163 "model.ump"
 public class CharacterCard extends Card
 {
 
-  //------------------------
-  // MEMBER VARIABLES
-  //------------------------
+    //------------------------
+    // MEMBER VARIABLES
+    //------------------------
 
-  //CharacterCard Attributes
-  private String name;
+    //CharacterCard Attributes
+    private String name;
+    Character character;
 
-  //------------------------
-  // CONSTRUCTOR
-  //------------------------
+    //------------------------
+    // CONSTRUCTOR
+    //------------------------
 
-  public CharacterCard(String name)
-  {
-    super(name);
-    this.name = name;
-  }
+    public CharacterCard(String name, Character character)
+    {
+        super(name);
+        this.name = name;
+        this.character = character;
+    }
 
-  //------------------------
-  // INTERFACE
-  //------------------------
+    //------------------------
+    // INTERFACE
+    //------------------------
 
 
-  public String getName()
-  {
-    return name;
-  }
+    public String getName()
+    {
+        return name;
+    }
 
-  @Override
-  public boolean equals(Object obj){
-    if (this == obj)  return true;
-    if (obj == null)  return false;
-    if (obj.getClass() != this.getClass())  return false;
-    CharacterCard other = (CharacterCard)obj;
-    return this.name.equals(other.name);
-  }
+    public Character getCharacter(){
+        return character;
+    }
 
-  @Override
-  public String toString()
-  {
-    return " ------------------- \n" +
-            " Card: " + name + "\n" +
-            " -------------------\n";
-  }
+    public String toString()
+    {
+        return " ------------------- \n" +
+                " Card: " + name + "\n" +
+                " ------------------- \n";
+    }
 }
