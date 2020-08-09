229710e0a8208324a94ad8fc5f8e8e042f669b5f
Bryonych <49720607+Bryonych@users.noreply.github.com>
2020-08-09 20:17:29 +1200

    Add javadoc

--- src/CharacterCard.java
+++ src/CharacterCard.java
@@ -1,43 +1,36 @@
+/**
+ * Respresents a character card in the game
+ */
 public class CharacterCard extends Card
 {
 
-    //------------------------
-    // MEMBER VARIABLES
-    //------------------------
+  private String name;
+  Character character;
 
-    //CharacterCard Attributes
-    private String name;
-    Character character;
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
 
-    //------------------------
-    // CONSTRUCTOR
-    //------------------------
+  public CharacterCard(String name, Character character)
+  {
+    super(name);
+    this.name = name;
+    this.character = character;
+  }
 
-    public CharacterCard(String name, Character character)
-    {
-        super(name);
-        this.name = name;
-        this.character = character;
-    }
+  public String getName()
+  {
+    return name;
+  }
 
-    //------------------------
-    // INTERFACE
-    //------------------------
+  public Character getCharacter(){
+    return character;
+  }
 
-
-    public String getName()
-    {
-        return name;
-    }
-
-    public Character getCharacter(){
-        return character;
-    }
-
-    public String toString()
-    {
-        return " ------------------- \n" +
-                " Card: " + name + "\n" +
-                " ------------------- \n";
-    }
+  public String toString()
+  {
+    return " ------------------- \n" +
+            " Card: " + name + "\n" +
+            " ------------------- \n";
+  }
 }
